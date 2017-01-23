55c36c6904619bba4037db0030e2b2c58aae84fa
Pieter Engelbrecht <pieter.engelbrecht@derivco.co.za>
2017-01-23 10:01:59 +0200

    Constructor Injection This is the most common DI. Dependency Injection is done by supplying the DEPENDENCY through the class’s constructor when instantiating that class. Injected component can be used anywhere within the class. Should be used when the injected dependency is required for the class to function. It addresses the most common scenario where a class requires one or more dependencies.

---
+++ DI/DI/Constructor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI
+{
+    public interface IService
+    {
+        void Serve();
+    }
+
+    public class Service : IService
+    {
+        public void Serve()
+        {
+            Console.WriteLine("Service has been called");
+        }
+    }
+
+    public class Client
+    {
+        private IService _service;
+
+        public Client(IService service)
+        {
+            this._service = service;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Service Started");
+            this._service.Serve();
+            //To Do: Some Stuff
+        }
+    }
+
+
+}
---
+++ DI/DI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DI
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Client client = new Client(new Service());
+            client.Start();
+            Console.ReadKey();
+
+        }
+    }
+}
